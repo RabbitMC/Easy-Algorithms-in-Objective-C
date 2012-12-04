42702ac3fcce41803e406d01c497d09770708b80
Miralem Cebic <m.cebic@rabbit-mobile.de>
2012-12-04 12:58:10 +0100

    Initial Commit

---
+++ Algorithm/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by Miralem Cebic on 04.12.12.
+//  Copyright (c) 2012 Miralem Cebic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
